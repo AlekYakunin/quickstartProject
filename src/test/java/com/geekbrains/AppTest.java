1a97f059595c7087f91ff0b8231df81dc635c7f2
Dmitry Yakunin <yakunin_dmitry@irand.ru>
2022-01-25 07:31:27 +0300

    Initial commit

---
+++ src/test/java/com/geekbrains/AppTest.java
@@ -0,0 +1,26 @@
+package com.geekbrains;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( 1 == 1 );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue1()
+    {
+        assertTrue( 1 == 2 );
+    }
+}
